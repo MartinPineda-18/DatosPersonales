2dd81ee15d02e7c789bcbc1c7db7bbc6612d0bf1
Juan Pineda <jmpinedarubiano@gmail.com>
2024-09-30 18:02:10 -0500

    Datos

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //git status
+        //git add .
+        //git commit -m " *mensaje que quiero cargar* "
+        //git push origin main
+
+        //wamp-28
+
+        //Juan Martin Pineda Rubiano
+        //juan.pineda-r@uniminuto.edu.co
+        //Zipaquira - El reposo
+        //Tenis, ciclismo, escuchar musica.
+
+
+    }
+}
